2d18e09f2f3ec895521ad83bb4fb74994ddb7e2f
akshaymungekar95 <akshaymungekar95@outlook.com>
2023-10-13 17:16:06 +0100

    updated BasePage to remove redundant packages

--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -1,6 +1,5 @@
 package pageObjects;
 
-import helper.WaitHelper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
